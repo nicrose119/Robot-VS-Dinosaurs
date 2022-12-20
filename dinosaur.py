b1b8b4491b43e7d63b086cae77112624809b3558
nicrose119 <nicrose119@gmail.com>
2022-12-20 13:06:07 -0500

    "Modified dinosaur name and attack power in the initializer. Defined attack and health functions in the dinosaur.py file"

--- dinosaur.py
+++ dinosaur.py
@@ -1,8 +1,16 @@
 class Dinosaur:
     def __init__(self, name, attack_power):
-        self.name = name()
-        self.attack_power = attack_power(20)
+        self.dinosaur = name()
+        self.attack_power = attack_power(10)
         self.dino_health = 100
 
     def attack(self, robot):
-        pass
+        if self.dino_health >0:
+            print(f"{self.name} attacks {robot.name}")
+       
+    def dino_health(self):
+        return self.dino_health >= 0
+    
+
+
+

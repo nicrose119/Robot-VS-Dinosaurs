0edc6dff7b2b826bdef1afb85af53966aa67d074
nicrose119 <nicrose119@gmail.com>
2022-12-20 12:59:15 -0500

    "Updated robot.py file by defining the attack and health functions"

--- robot.py
+++ robot.py
@@ -3,8 +3,12 @@ from weapon import Weapon
 class Robot:
     def __init__(self, name):
         self.name = name()
-        self.health = 100
-        self.active_weapon = Weapon("Laser Sword", 15)
+        self.robot_health = 100
+        self.active_weapon = Weapon("Laser Sword", 10)
 
-    def attack(self,dinosaur):
-        pass      
+    def attack(self, dinosaur):
+        if self.robot_health >0:
+            print(f"{self.name} attacks {dinosaur.name}")
+       
+    def robot_health (self):
+        return self.health >= 0   
